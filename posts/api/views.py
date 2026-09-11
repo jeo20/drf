9223745c77ccd48c94dc83059683fabd94c69861
Jorge Mefi <jeo20@hotmail.com>
2026-09-11 15:53:29 -0300

    005 Serializadores

--- posts/api/views.py
+++ posts/api/views.py
@@ -2,16 +2,17 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from posts.api.serialisers import PostSerializer
 from posts.models import Post
 
 
 class PostApiView(APIView):
     def get(self, request):
-        posts = [post.title for post in Post.objects.all()]
-        return Response(status=status.HTTP_200_OK, data=posts)
+        serializer = PostSerializer(Post.objects.all(), many=True)
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
 
     def post(self, request):
-        Post.objects.create(title=request.POST.get("title"),
-                            description=request.POST.get("description"),
-                            order=request.POST.get("order"))
-        return self.get(request)
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
