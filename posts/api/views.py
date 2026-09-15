b97d25c2778153fbaa955d9a522c7374aa1a2ddb
Jorge Mefi <jeo20@hotmail.com>
2026-09-15 13:21:33 -0300

    06 viewset

--- posts/api/views.py
+++ posts/api/views.py
@@ -1,18 +1,22 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet
 
 from posts.api.serializers import PostSerializer
 from posts.models import Post
 
 
-class PostApiView(APIView):
-    def get(self, request):
+class PostViewSet(ViewSet):
+    def list(self, request):
         serializer = PostSerializer(Post.objects.all(), many=True)
         return Response(status=status.HTTP_200_OK, data=serializer.data)
+    
+    def retrieve(self, request, pk: int):
+        serializer = PostSerializer(Post.objects.filter(pk=pk), many=True)
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
 
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
+    def create(self, request):
+        serializer = PostSerializer(data=request.POST)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(status=status.HTTP_200_OK, data=serializer.data)
