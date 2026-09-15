cb6374fa851c123ec12cac0cb807937bf55bda33
Jorge Mefi <jeo20@hotmail.com>
2026-09-15 11:07:17 -0300

    005 serializadores ok

--- posts/api/views.py
+++ posts/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from posts.api.serialisers import PostSerializer
+from posts.api.serializers import PostSerializer
 from posts.models import Post
 
 
--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,18 @@
-from django.http import request
-from django.shortcuts import render
-from django.views.generic.base import View
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from posts.api.serializers import PostSerializer
+from posts.models import Post
 
 
-class HelloWorld(View):
+class PostApiView(APIView):
     def get(self, request):
-        data = {
-            'name': 'Jorge Orellana',
-            'years': 30,
-            'codes': ['Python', 'JavaScript', 'Java', 'C#']
-        }
-        return render(request, 'hello_world.html', context=data)
+        serializer = PostSerializer(Post.objects.all(), many=True)
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.POST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
