ed8dc47beb43d7cc04bc0779384b9329e7321f40
Jorge Mefi <jeo20@hotmail.com>
2026-09-15 13:59:44 -0300

    07 modelviewset

--- posts/api/views.py
+++ posts/api/views.py
@@ -1,11 +1,15 @@
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ModelViewSet
 
 from posts.api.serializers import PostSerializer
 from posts.models import Post
 
 
+class PostModelViewSet(ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+
+"""
 class PostViewSet(ViewSet):
     def list(self, request):
         serializer = PostSerializer(Post.objects.all(), many=True)
@@ -20,3 +24,4 @@ class PostViewSet(ViewSet):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(status=status.HTTP_200_OK, data=serializer.data)
+"""
