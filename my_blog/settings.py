c47ddb5ab603db34fe25728e7996ba7be178a56f
Jorge Mefi <jeo20@hotmail.com>
2026-09-16 15:46:05 -0300

    configuracion drf-yasg

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
-    'posts'
+    "drf_yasg",
+    "posts"
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = './static/'
 
 
 # Email
